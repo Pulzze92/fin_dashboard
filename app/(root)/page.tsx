a89e52544964861ff8399e37818811923033df37
Pulzze92 <76836870+Pulzze92@users.noreply.github.com>
2024-11-07 22:42:12 +0100

    feat: added header box and accounts box

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import TotalBalanceBox from "@/components/TotalBalanceBox"
+import HeaderBox from '@/components/HeaderBox'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Alex' };
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalInvestingAccounts={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
