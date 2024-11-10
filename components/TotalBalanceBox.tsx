7410858d186d1d2e11768d5d17a51742edfc4064
Pulzze92 <76836870+Pulzze92@users.noreply.github.com>
2024-11-10 23:00:48 +0100

    feat: added animation counter

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,5 +1,5 @@
-import { formatAmount } from '@/lib/utils'
 import React from 'react'
+import AnimatedCounter from './AnimatedCounter'
 
 function TotalBalanceBox({
     accounts = [],
@@ -20,7 +20,7 @@ function TotalBalanceBox({
                     Total Current Balance
                 </p>
                 <p className='total-balance-amount flex-center gap-2'>
-                    {formatAmount(totalCurrentBalance)}
+                    <AnimatedCounter amount={totalCurrentBalance}/>
                 </p>
             </div>
         </div>
